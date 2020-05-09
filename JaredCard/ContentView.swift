b04febcfeca66c73221c2b43962e0aed3f9a909d
Jared Borders <jlbxsxs@gmail.com>
2020-05-08 19:51:07 -0500

    Initial Commit

---
+++ JaredCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JaredCard
+//
+//  Created by Jared Borders on 5/8/20.
+//  Copyright © 2020 Jared Borders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
